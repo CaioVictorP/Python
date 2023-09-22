cd2a5b2e39eb5c776d43484a63962d7d5125d376
CaioVictorP <101927647+CaioVictorP@users.noreply.github.com>
2023-09-22 08:50:57 -0300

    a

--- Aula.py
+++ Aula.py
@@ -1,1 +1,22 @@
-print("a")
+while True:
+    nome = input("Digite o seu nome: ")
+    estciv = input("Digite o seu estado civil(S/C/V/D): ")
+    estciv.upper()
+    if estciv != "S" and estciv != "C" and estciv != "V" and estciv != "D":
+        print("Digite apenas S/C/V/D!")
+    else:
+        if estciv == "S":
+            estciv = "Solteiro/a"
+        elif estciv == "C":
+            estciv = "Casado/a"
+        elif estciv == "V":
+            estciv = "Viuvo/a"
+        else:
+            estciv == "Divorciado/a"
+        print(f"{nome} é {estciv}")
+        final = int("Digite 1 para terminar e 2 para repetir ")
+        if final == 2:
+            print("Recomeçando...")
+        else:
+            print("Terminando")
+            break
