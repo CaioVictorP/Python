8b07a73778ddbc38db70cbb49dada88a24e54f8c
CaioVictorP <101927647+CaioVictorP@users.noreply.github.com>
2023-10-27 12:09:49 +0000

    y

--- Aula 27/10/Aula 1.py
+++ Aula 27/10/Aula 1.py
@@ -4,7 +4,7 @@ while True:
     metodo = input("Digite a operação: (+, -, *, /) ")
     if metodo == "+" or metodo == "-" or metodo == "*" or metodo == "/":
         resultado = eval(num1 + metodo + num2)
-        print(resultado)
+        print(f"{resultado:.2f}")
     else:
         print("Operação inválida! Coloque apenas +, -, * ou /!")
     end = input("Deseja continuar? (S/N): ")
