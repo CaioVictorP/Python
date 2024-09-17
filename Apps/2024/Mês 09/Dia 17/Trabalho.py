f9af037fe9776b32dd5e5b8e28ff90414454c6bf
CaioVictorP <101927647+CaioVictorP@users.noreply.github.com>
2024-09-17 10:17:29 -0300

    aa

--- Apps/2024/Mês 09/Dia 17/Trabalho.py
+++ Apps/2024/Mês 09/Dia 17/Trabalho.py
@@ -8,12 +8,8 @@ nome = input("Digite o nome do abrigado: ")
 cpf = input("Digite o CPF do abrigado: ")
 cursor.execute("INSERT INTO Abrigo VALUES ('"+nome+"', '"+cpf+"', 0)")
 
-rows = cursor.execute("SELECT * FROM Abrigo").fetchall()
-print(rows)
-
 newscore = input("Digite a nova pontuação: ")
 cpfs = input("Digite o seu CPF para salvar sua pontuação: ")
 save = cursor.execute("UPDATE Abrigo SET pontuacao = "+ newscore +" WHERE cpf = "+ cpfs)
 
-rows2 = cursor.execute("SELECT * FROM Abrigo").fetchall()
-print(rows2)
+connection.commit()
